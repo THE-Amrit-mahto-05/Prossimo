3b3aec2b7ef6dc26ffd944bf2ea4eefb79649f0e
Amrit kumar Mahto <amrit.mahto@adypu.edu.in>
2026-04-19 20:22:48 +0530

    schema added for database

--- src/infrastructure/repositories/UserRepository.ts
+++ src/infrastructure/repositories/UserRepository.ts
@@ -1,10 +1,8 @@
 import { AbstractRepository } from './AbstractRepository';
 import { User } from '../../domain/User';
 
-// Inheritance and Realization
 export class UserRepository extends AbstractRepository<User> {
-    
-    // Subtype specific implementation realizing the abstract methods
+
     public async update(id: string, entity: User): Promise<void> {
         if (!this.memoryStorage.has(id)) {
             throw new Error("User not found");
